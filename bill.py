872067c48f1368565525e480ef390146cc49a1db
sai teja <venkatsaiteja1027@gmail.com>
2021-10-17 00:03:28 +0530

    discount opiton added

--- bill.py
+++ bill.py
@@ -1,12 +1,12 @@
 # here bill is generated.
+from res import *
+def bill(newBag, member):
 
-def bill(newBag):
-
-    print('-'*50)
-    print(' '*10,"TEJA SUPER MARKET")
-    print('-'*50)
-    print('\n')
-    print('{:<10} {:<10} {:<10} {:<10} {:<10}'.format("S.NO" ,"ITEM" ,"MRP", "QUANTITY" ,"PRICE"))
+    line()
+    print(' '*15,"TEJA SUPER MARKET\n")
+    print(f'Date : {day()}       Time : {time()}')
+    line()
+    print('{:<10} {:<10} {:<10} {:<10} {:<10}\n'.format("S.NO" ,"ITEM" ,"MRP", "QUANTITY" ,"PRICE"))
 
     sno=0
     billAmount=0
@@ -16,9 +16,17 @@ def bill(newBag):
         print('{:<10} {:<10} {:<10} {:<10} {:<10}'.format( sno,i[0] ,i[1], i[2] ,i[1]*i[2]))
         billAmount+=i[1]*i[2]
 
-    print()
-    print('-'*50)
-    print(' '*30,"total bill = ",billAmount,' '*4)
-    print('-'*50)
-    print('thank u visit again  :)')
-    print('\n\n')
+    print("-"*55)
+    print('{:>45}'.format(f'Bill = {billAmount}'))
+
+    if member :
+        Discount=discount(billAmount)
+        print('{:>45}'.format(f'Discount = - {Discount}'))
+        billAmount-=Discount
+
+    Tax=tax(billAmount)
+    print('{:>35}'.format(f'Tax (CGST + SGST) = + {Tax}'))
+    billAmount+=Tax
+    print('{:>35}'.format(f'Total Bill = {billAmount}'))
+    line()
+    print('Thank u visit again  :)')
